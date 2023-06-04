a06f12097af86fb5d0c84a6191316f28c114512f
mattsloe <mattsloe@gmail.com>
2023-06-04 01:32:55 -0700

    Fixed hamburger menu. It works now! Changed color to suit both dark and light backgrounds

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,11 +19,11 @@ function closer() {
 //open when someone clicks on the hamburger icon
 function openNav() {
     if (flag) {
-        document.getElementByID("myNav").style.height = "100%";
+        document.getElementById("myNav").style.height = "100%";
         $(".line").css("background", "white");
         flag = false; //to close the menu
     } else {
-        document.getElementByID("myNav").style.height = "0%";
+        document.getElementById("myNav").style.height = "0%";
         $(".line").css("background", "black");
         flag = true;
     }
